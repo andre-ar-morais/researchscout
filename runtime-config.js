87da39fe54df444d7a9e0f69c8ea0f8bf70c6656
André Morais <andremorais.analytics@gmail.com>
2026-05-05 20:16:56 -0300

    Add files via upload

---
+++ runtime-config.js
@@ -0,0 +1,6 @@
+// Runtime configuration for static hosting (GitHub Pages, Cloudflare Pages, etc).
+window.NEXUS_RUNTIME = {
+  FORM_API_URL:   "https://script.google.com/macros/s/AKfycbyTrjAcujgzLCSmJaWPJjUbPobFaKePSBPwnjKCIvrabNUN9Ezzj01L6vvWaTh1IN_s/exec",
+  SECRET_TOKEN:   "275c555c81666780aa3a995b1aa24f9c0bb2bda3"
+};
+ 
